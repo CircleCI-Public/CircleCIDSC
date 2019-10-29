7f40bb7b60c831da2fb48ada5435c66abcc79a61
Eric Fode <ericfode@circleci.com>
2019-10-29 15:22:21 -0700

    try it with some docker

--- Tests/Integration/CircleChoco.Integration.Tests.ps1
+++ Tests/Integration/CircleChoco.Integration.Tests.ps1
@@ -1,3 +1,5 @@
+[Microsoft.DscResourceKit.IntegrationTest(OrderNumber = 1, ContainerName = 'windows', ContainerImage = 'mcr.microsoft.com/windows/servercore:ltsc2019')]
+param()
 <#
     .SYNOPSIS
        Integration test for CircleChoco module
