a67847b66f0cd4b2e4182e6d74e0ae87ff0811ee
Eric Fode <ericfode@circleci.com>
2019-11-01 10:57:42 -0700

    magic sadness

--- DSCResources/CirclePath/CirclePath.schema.psm1
+++ DSCResources/CirclePath/CirclePath.schema.psm1
@@ -30,17 +30,16 @@ Configuration CirclePath {
             $currentPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
             $newPath = $using:PathItem + ';' + $currentPath
             Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
-            Write-Verbose -Message "starting Env: $Env"
             foreach($level in "Machine","User") {
                 [Environment]::GetEnvironmentVariables($level).GetEnumerator() | ForEach-Object {
                     # For Path variables, append the new values, if they're not already in there
-                    if($_.Name -match 'Path$') {
+                    if($_.Name -match '^Path$') {
                         $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select-Object -unique) -join ';'
                     }
+                    Write-Verbose $_
                     $_
                 } | Set-Content -Path { "Env:$($_.Name)" }
             }
-            Write-Verbose -Message "Ending Env: $Env"
 
         }
     }
