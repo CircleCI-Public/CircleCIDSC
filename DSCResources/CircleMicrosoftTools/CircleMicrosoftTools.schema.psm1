855a9c7e4f721cc40d5633d0cc1fc1e78ba80da7
mrothstein74 <michael.rothstein@circleci.com>
2021-11-11 15:33:06 -0800

    windows: fixing sql server install

--- DSCResources/CircleMicrosoftTools/CircleMicrosoftTools.schema.psm1
+++ DSCResources/CircleMicrosoftTools/CircleMicrosoftTools.schema.psm1
@@ -152,6 +152,7 @@ Configuration CircleMicrosoftTools {
     cChocoPackageInstaller sqlserverexpress
     {
         Name      = "sql-server-express"
+        Ensure    = 'Present'
         Version   = "2019.20200409"
         Params    = "-o -ia '/IACCEPTSQLSERVERLICENSETERMS /ACTION=install /INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER /UPDATEENABLED=FALSE /SECURITYMODE=SQL /SAPWD=r22rbf8*PUHjqzb3' -force -yes"
         DependsOn = "[CircleChoco]choco"
