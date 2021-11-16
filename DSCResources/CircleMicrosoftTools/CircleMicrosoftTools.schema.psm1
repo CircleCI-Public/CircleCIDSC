9a39b82086050e2fc018a37ae272e23cf6538c67
mrothstein74 <michael.rothstein@circleci.com>
2021-11-15 16:16:05 -0800

    trying to fix sql server install

--- DSCResources/CircleMicrosoftTools/CircleMicrosoftTools.schema.psm1
+++ DSCResources/CircleMicrosoftTools/CircleMicrosoftTools.schema.psm1
@@ -30,7 +30,7 @@ Configuration CircleMicrosoftTools {
         Name      = "sql-server-express"
         Ensure    = 'Present'
         Version   = "2019.20200409"
-        Params    = "--cachelocation=c:\tmp"
+        Params    = "-c='c:\tmp\'"
     }
 
 
