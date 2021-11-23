1e5b4f3295884f411c3025626fbeac2ed5ff2f5b
mrothstein74 <michael.rothstein@circleci.com>
2021-11-22 22:12:41 -0800

    testing CommonTestHelper.ps1

--- Tests/Integration/CircleMicrosoftTools.Integration.Tests.ps1
+++ Tests/Integration/CircleMicrosoftTools.Integration.Tests.ps1
@@ -2,6 +2,8 @@ $script:dscModuleName = 'CircleCIDSC'
 $script:dscResourceFriendlyName = 'CircleMicrosoftTools'
 $script:dscResourceName = "$($script:dscResourceFriendlyName)"
 
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+
 #region HEADER
 # Integration Test Template Version: 1.3.3
 [String] $script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
