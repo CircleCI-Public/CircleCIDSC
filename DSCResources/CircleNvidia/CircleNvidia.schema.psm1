d3f50b500f58a209b88921d19159aec48133383b
mrothstein74 <michael.rothstein@circleci.com>
2022-01-18 11:17:44 -0500

    reverting to cuda 10

--- DSCResources/CircleNvidia/CircleNvidia.schema.psm1
+++ DSCResources/CircleNvidia/CircleNvidia.schema.psm1
@@ -3,13 +3,9 @@ Configuration CircleNvidia {
 
     cChocoPackageInstaller installNvidia {
         Name = 'cuda'
-        Version = '11.5.0.49613'
+        Version = '10.1.243'
         Source = 'chocolatey'
     }
-
-    Write-Output "CUDA INSTALL LOG:"
-    Get-Content C:\ProgramData\chocolatey\logs\chocolatey.log
-    Write-Output "END CUDA INSTALL LOG:"
     
     Script InstallCudnn {
         GetScript = {
@@ -21,9 +17,9 @@ Configuration CircleNvidia {
         }
 
         TestScript = {
-            return ($(Test-Path -Path 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.5\bin\cudnn64_7.dll') `
-              -and $(Test-Path -Path 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.5\include\cudnn.h') `
-              -and $(Test-Path -Path 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.5\lib\x64\cudnn.lib'))
+            return ($(Test-Path -Path 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin\cudnn64_7.dll') `
+              -and $(Test-Path -Path 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\include\cudnn.h') `
+              -and $(Test-Path -Path 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\lib\x64\cudnn.lib'))
         }
 
         SetScript = {
@@ -32,9 +28,9 @@ Configuration CircleNvidia {
             (New-Object System.Net.WebClient).DownloadFile($url, $output)
             Expand-Archive -Path $output -DestinationPath C:\cudnn
             
-            cp C:\cudnn\cuda\bin\cudnn64_7.dll 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.5\bin'
-            cp C:\cudnn\cuda\include\cudnn.h 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.5\include'
-            cp C:\cudnn\cuda\lib\x64\cudnn.lib 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.5\lib\x64'
+            cp C:\cudnn\cuda\bin\cudnn64_7.dll 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin'
+            cp C:\cudnn\cuda\include\cudnn.h 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\include'
+            cp C:\cudnn\cuda\lib\x64\cudnn.lib 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\lib\x64'
         }
 
         DependsOn = "[cChocoPackageInstaller]installNvidia"
